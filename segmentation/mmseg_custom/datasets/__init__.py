fa59f0d4fc3bfeb7be72d3557f69ad14e06fe68f
Hyggge <hyggge6@gmail.com>
2023-12-31 10:35:45 +0800

    feat: 增加 BDD100k drivable 数据集

--- segmentation/mmseg_custom/datasets/__init__.py
+++ segmentation/mmseg_custom/datasets/__init__.py
@@ -2,4 +2,5 @@
 from .mapillary import MapillaryDataset  # noqa: F401,F403
 from .potsdam import PotsdamDataset  # noqa: F401,F403
 from .cityscapes_road import CityscapesRoadDataset # noqa: F401,F403
+from .bdd100k_drivable import BDD100kDrivableDataset # noqa: F401,F403
 from .pipelines import *  # noqa: F401,F403
