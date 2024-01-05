cde609b99173a80368500c0354e1fda097c0685d
Hyggge <hyggge6@gmail.com>
2024-01-05 07:36:33 +0000

    feat: 更新 my_train.sh 和 my_test.sh, 训练时随机种子固定为42, 测试时输出分割图

--- segmentation/my_test.sh
+++ segmentation/my_test.sh
@@ -1,5 +1,5 @@
 CONFIG=$1
 CKPT=$2
 
-srun -p normal -w irip-c3-compute-2 --gres=gpu:3080ti:1   -c 16 --mem 16G \
-python test.py ${CONFIG} ${CKPT}  --eval mIoU --work-dir "work/test" 
+srun -p normal -w irip-c3-compute-1 --gres=gpu:3090:1   -c 16 --mem 16G \
+python test.py ${CONFIG} ${CKPT}  --eval mIoU --work-dir "work/test/test_log" --out "work/test/test_out.pkl" --show-dir "work/test/test_show"
