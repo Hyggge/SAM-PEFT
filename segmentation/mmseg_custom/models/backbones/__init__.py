b563378da28991977956cf9f48f84d3a3a172a8b
Hyggge <hyggge6@gmail.com>
2024-01-10 15:36:37 +0800

    fix: 构建 SAM_VPT, 并添加相关配置文件

--- segmentation/mmseg_custom/models/backbones/__init__.py
+++ segmentation/mmseg_custom/models/backbones/__init__.py
@@ -5,6 +5,7 @@ from .vit_adapter import ViTAdapter
 from .vit_baseline import ViTBaseline
 from .uniperceiver_adapter import UniPerceiverAdapter
 from .sam_adapter import SAMAdapter
+from .sam_vpt import SAMVPT
 
 __all__ = ['ViTBaseline', 'ViTAdapter', 'BEiTAdapter',
            'BEiTBaseline', 'UniPerceiverAdapter']
