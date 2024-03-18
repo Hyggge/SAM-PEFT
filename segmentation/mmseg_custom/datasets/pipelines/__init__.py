e0043ef8cca1a70ad9f9c3ad915f4d7dfd783c91
Hyggge <hyggge6@gmail.com>
2024-03-18 11:46:04 +0800

    fix: 增加LoadCustomRoadMarkGT, 可以根据需要对道路标记分割图的label进行修改

--- segmentation/mmseg_custom/datasets/pipelines/__init__.py
+++ segmentation/mmseg_custom/datasets/pipelines/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .formatting import DefaultFormatBundle, ToMask, MultiHeadCollect
 from .transform import MapillaryHack, PadShortSide, SETR_Resize
-from .load_auxiliary_gt import LoadLaneDetectGT, LoadRoadMarkGT, LoadBinRoadMarkGT
+from .load_auxiliary_gt import LoadLaneDetectGT, LoadRoadMarkGT, LoadBinRoadMarkGT, LoadCustomRoadMarkGT
 
 __all__ = [
     'DefaultFormatBundle', 'ToMask', 'SETR_Resize', 'PadShortSide',
-    'MapillaryHack', 'LoadLaneDetectGT', 'LoadRoadMarkGT', 'MultiHeadCollect', 'LoadBinRoadMarkGT'
+    'MapillaryHack', 'LoadLaneDetectGT', 'LoadRoadMarkGT', 'MultiHeadCollect', 'LoadBinRoadMarkGT', 'LoadCustomRoadMarkGT'
 ]
--- segmentation/mmseg_custom/datasets/pipelines/load_auxiliary_gt.py
+++ segmentation/mmseg_custom/datasets/pipelines/load_auxiliary_gt.py
@@ -124,3 +124,46 @@ class LoadBinRoadMarkGT(object):
     def __repr__(self):
         return self.__class__.__name__ + \
                f'(ignore_index={self.ignore_index})'
+
+@PIPELINES.register_module()
+class LoadCustomRoadMarkGT(object):
+    """Transfer gt_semantic_seg to binary mask and generate gt_labels."""
+    def __init__(self, aux_gt_dirs, suffixs, id_map=None):
+        self.aux_gt_dirs = aux_gt_dirs
+        self.suffixs = suffixs
+        self.file_client = mmcv.FileClient(backend='disk')
+        self.id_list = [0, 200, 204, 213, 209, 206, 207, 201, 203, 211, 208, 216, 217,
+            215, 218, 219, 210, 232, 214, 202, 220, 221, 222, 231, 224, 225,
+            226, 230, 228, 229, 233, 205, 212, 227, 223, 250, 249, 255]
+        if id_map is None:
+            self.id_map = {k : v for k, v in zip(self.id_list[1:-2], range(1, 36))}
+            self.id_map[0] = 0
+            self.id_map[249] = 0
+            self.id_map[255] = 255
+
+        else:
+            self.id_map = id_map
+
+
+    def __call__(self, results):
+        for i, (dir, suffix) in enumerate(zip(self.aux_gt_dirs, self.suffixs)):
+            filename = os.path.join(dir, results['img_info']['filename'])
+            filename = os.path.splitext(filename)[0] + suffix
+            img_bytes = self.file_client.get(filename)
+            aux_gt = mmcv.imfrombytes(
+                img_bytes, flag='grayscale', backend='cv2').squeeze().astype(np.uint8)
+            # convert to train ID
+            for k, v in self.id_map.items():
+                aux_gt[aux_gt == k] = v
+            # aux_gt_list.append(aux_gt)
+            results[f'aux_gt_{i+1}'] = aux_gt
+            results['seg_fields'].append(f'aux_gt_{i+1}')
+
+        # print(f"{results.keys()=}")
+        # print(f"{results['img_info'].keys()=}")
+        # print(results)
+        return results
+
+    def __repr__(self):
+        return self.__class__.__name__ + \
+               f'(ignore_index={self.ignore_index})'
