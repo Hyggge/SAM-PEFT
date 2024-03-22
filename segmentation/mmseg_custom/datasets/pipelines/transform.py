b3661c6b91fe480a2443f45b6502109b8c501bc6
Hyggge <hyggge6@gmail.com>
2024-03-22 10:50:54 +0800

    feat: 当 road mark 作为先验引入时, 将像素值归一化到[0, 1]

--- segmentation/mmseg_custom/datasets/pipelines/transform.py
+++ segmentation/mmseg_custom/datasets/pipelines/transform.py
@@ -348,3 +348,46 @@ class MapillaryHack(object):
     def __repr__(self):
         repr_str = self.__class__.__name__
         return repr_str
+
+
+@PIPELINES.register_module()
+class NormalizeCustom(object):
+    """Normalize the image.
+
+    Added key is "img_norm_cfg".
+
+    Args:
+        mean (sequence): Mean values of 3 channels.
+        std (sequence): Std values of 3 channels.
+        to_rgb (bool): Whether to convert the image from BGR to RGB,
+            default is true.
+    """
+
+    def __init__(self, field_name, mean, std, to_rgb=True):
+        self.mean = np.array(mean, dtype=np.float32)
+        self.std = np.array(std, dtype=np.float32)
+        self.field_name = field_name
+        self.to_rgb = to_rgb
+
+    def __call__(self, results):
+        """Call function to normalize images.
+
+        Args:
+            results (dict): Result dict from loading pipeline.
+
+        Returns:
+            dict: Normalized results, 'img_norm_cfg' key is added into
+                result dict.
+        """
+
+        results[self.field_name] = mmcv.imnormalize(results[self.field_name], self.mean, self.std,
+                                          self.to_rgb)
+        results[f'{self.field_name}_cfg'] = dict(
+            mean=self.mean, std=self.std, to_rgb=self.to_rgb)
+        return results
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(mean={self.mean}, std={self.std}, to_rgb=' \
+                    f'{self.to_rgb})'
+        return repr_str
