7406f991f5b444de5ea2a4e4afcbc09a086d6458
Hyggge <hyggge6@gmail.com>
2024-04-25 10:22:44 +0800

    feat: 将所有路面标记分割辅助任务实验的辅助损失设置为1.0

--- segmentation/configs/bdd100k_drivable/upernet_sam_adapter_vpt_multi_head_binary_road_mark_generated_base_640_160k_bdd100k_drivable.py
+++ segmentation/configs/bdd100k_drivable/upernet_sam_adapter_vpt_multi_head_binary_road_mark_generated_base_640_160k_bdd100k_drivable.py
@@ -68,7 +68,7 @@ model = dict(
             norm_cfg=norm_cfg,
             align_corners=False,
             loss_decode=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=5)
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
         ),
     ],
     test_cfg=dict(mode='whole')
