792c3a46449a7c3f5b4554d57a352b0c05289a4a
Hyggge <hyggge6@gmail.com>
2024-05-08 23:02:09 +0800

    feat: 增加专门用于主任务和辅助任务推理的 UPerHeadV2Infer

--- segmentation/mmseg_custom/models/decode_heads/__init__.py
+++ segmentation/mmseg_custom/models/decode_heads/__init__.py
@@ -2,9 +2,11 @@
 from .mask2former_head import Mask2FormerHead
 from .maskformer_head import MaskFormerHead
 from .uper_head_v2 import UPerHeadV2
+from .uper_head_v2_infer import UPerHeadV2Infer
 
 __all__ = [
     'MaskFormerHead',
     'Mask2FormerHead',
-    'UPerHeadV2'
+    'UPerHeadV2',
+    'UPerHeadV2Infer'
 ]
