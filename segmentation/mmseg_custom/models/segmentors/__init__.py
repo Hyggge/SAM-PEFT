cc707dfc02236b60c4415f35e3d79975da950aad
Hyggge <hyggge6@gmail.com>
2024-05-08 21:43:29 +0800

    feat: 增加专门用于主任务和辅助任务推理的 EncoderDecoderMultiHeadV2Infer

--- segmentation/mmseg_custom/models/segmentors/__init__.py
+++ segmentation/mmseg_custom/models/segmentors/__init__.py
@@ -4,6 +4,7 @@ from .encoder_decoder_mask2former_aug import EncoderDecoderMask2FormerAug
 from .encoder_decoder_multi_head import EncoderDecoderMultiHead
 from .encoder_decoder_multi_input import EncoderDecoderMultiInput
 from .encoder_decoder_multi_head_v2 import EncoderDecoderMultiHeadV2
+from .encoder_decoder_multi_head_v2_infer import EncoderDecoderMultiHeadV2Infer
 
 __all__ = ['EncoderDecoderMask2Former', 'EncoderDecoderMask2FormerAug', 'EncoderDecoderMultiHead', 'EncoderDecoderMultiInput',
-           'EncoderDecoderMultiHeadV2']
+           'EncoderDecoderMultiHeadV2', 'EncoderDecoderMultiHeadV2Infer']
