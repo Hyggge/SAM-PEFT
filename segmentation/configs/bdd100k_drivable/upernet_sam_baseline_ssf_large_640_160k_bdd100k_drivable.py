a780ff5302a1b716b757d00411c954ea3fbd5b53
Hyggge <hyggge6@gmail.com>
2024-05-19 20:39:54 +0800

    fix: 增加SAM-Large实验中的需要对比的方法配置

---
+++ segmentation/configs/bdd100k_drivable/upernet_sam_baseline_ssf_large_640_160k_bdd100k_drivable.py
@@ -0,0 +1,41 @@
+# Copyright (c) Hyggge. All rights reserved.
+_base_ = [
+    '../_base_/models/upernet_r50.py', '../_base_/datasets/bdd100k_drivable.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+# pretrained = 'https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth'
+pretrained = 'pretrained/sam_vit_l_0b3195.pth'
+model = dict(
+    pretrained=pretrained,
+    backbone=dict(
+        _delete_=True,
+        type='SAMBaselineSSF',
+        # SAM-L Parameters
+        encoder_embed_dim=1024,
+        encoder_depth=24,
+        encoder_num_heads=16,
+        encoder_global_attn_indexes=[5, 11, 17, 23],
+        ),
+    decode_head=dict(num_classes=150, in_channels=[1024, 1024, 1024, 1024]),
+    auxiliary_head=dict(num_classes=150, in_channels=1024),
+    test_cfg=dict(mode='whole')
+)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+
+optimizer = dict(_delete_=True, type='AdamW', lr=1e-4, betas=(0.9, 0.999), weight_decay=0.05,
+                 constructor='LayerDecayOptimizerConstructor',
+                 paramwise_cfg=dict(num_layers=24, layer_decay_rate=0.90))
+lr_config = dict(_delete_=True, 
+                 policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data=dict(samples_per_gpu=2)
+runner = dict(type='IterBasedRunner')
+checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
+evaluation = dict(interval=16000, metric='mIoU', save_best='mIoU')
+fp16 = dict(loss_scale=dict(init_scale=512))
